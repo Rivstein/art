7175ce7df30e241572dfab9215526d397997e010
Rivstein <51017849+Rivstein@users.noreply.github.com>
2020-08-20 19:11:14 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function toggle_promo(){
+	document.getElementById('promo_code_btn').style.display = "none";;
+	document.getElementById('promo_code_form').style.display = "block";;
+}
+
+function toggle_gift(){
+	document.getElementById('gift_code_btn').style.display = "none";;
+	document.getElementById('gift_code_form').style.display = "block";;
+}
